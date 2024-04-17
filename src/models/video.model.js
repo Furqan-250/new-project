dd7baee6c559229a4c9cbdfc3b78361bdd004aa0
muhammadumarrehan <umar@aiztek.com>
2024-04-18 01:15:52 +0500

    add user & video model

---
+++ src/models/video.model.js
@@ -0,0 +1,47 @@
+import { type } from "express/lib/response";
+import mongooseAggregatePaginate, { MongooseAggregatePaginate } from "mongoose-aggregate-paginate-v2";
+import { Mongoose, Schema } from "mongoose";
+
+const videoSchema = new Schema(
+  {
+    videoFilte: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = Mongoose.Model("Video", videoSchema);
